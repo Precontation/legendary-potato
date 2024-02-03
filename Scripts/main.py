11b5ff69a74511136e15e6249f76a3decdddf9be
Max <supkittymeow@gmail.com>
2024-02-02 20:01:08 -0800

    ads move b etter

--- Scripts/main.py
+++ Scripts/main.py
@@ -8,17 +8,19 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.init()
 
 character = player.Player()
-animationCycle = 0
-shouldChangeAnim = 0
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill('black')
+    screen.fill('white')
 
-    character.move(pygame.key.get_pressed(), character.playerDirection, animationCycle, character.keysPressed)
+    playerDirection = character.playerDirection
+    character.move(pygame.key.get_pressed(), character.playerDirection, player.animationCycle)
+    character.update(6)
     screen.blit(character.image, (character.rect.x, character.rect.y))
+
     pygame.display.flip()
+    pygame.time.Clock().tick(60) / 1000
 pygame.quit()
--- Scripts/player.py
+++ Scripts/player.py
@@ -1,51 +1,35 @@
 import pygame
 
-playerDirection = 'Up'
-moveSpeed = 5
-
-keysPressed = []
+animationCycle = 1
 
 class Player(pygame.sprite.Sprite):
     def __init__(self) -> None:
         super().__init__()
         self.image = pygame.transform.scale_by(pygame.image.load('Images/Player/Up/Up.png'), 10)
         self.rect = self.image.get_rect()
-    def move(self, keys, playerDirection, animationCycle, keysPressed):
-        if keys[pygame.K_w]:
-            if not keysPressed.__contains__('Up'):
-                playerDirection = 'Up'
-                keysPressed += "Up"
-            self.rect.y -= moveSpeed
-        # else: keysPressed -= 'Up'
-        if keys[pygame.K_s]:
-            if not keysPressed.__contains__('Down'):
-                playerDirection = 'Down'
-                keysPressed += 'Down'
-            self.rect.y += moveSpeed
-        # else: keysPressed -= 'Down'
-        if keys[pygame.K_a]:
-            if not keysPressed.__contains__('Left'):
-                playerDirection = 'Left'
-                keysPressed += 'Left'
-            self.rect.x -= moveSpeed
-        # else: keysPressed -= 'Left'
-        if keys[pygame.K_d]:
-            if not keysPressed.__contains__('Right'):
-                playerDirection = 'Right'
-                keysPressed += 'Right'
-            self.rect.x += moveSpeed
-        # else: keysPressed -= 'd'
+        self.playerDirection = 'Down'
+        self.moveSpeed = 5
+    def update(self, speed) -> None:
+        if animationCycle >= 2:
+            animationCycle = 1
+        else: 
+            animationCycle += 1
+        pygame.time.Clock().tick(speed) / 1000  
+    def move(self, keys, playerDirection, animationCycle):
+        if keys[pygame.K_w or pygame.K_UP]:
+            self.playerDirection = 'Up'
+            self.rect.y -= self.moveSpeed
+        if keys[pygame.K_s or pygame.K_DOWN]:
+            self.playerDirection = 'Down'
+            self.rect.y += self.moveSpeed
+        if keys[pygame.K_a or pygame.K_LEFT]:
+            self.playerDirection = 'Left'
+            self.rect.x -= self.moveSpeed
+        if keys[pygame.K_d or pygame.K_RIGHT]:
+            self.playerDirection = 'Right'
+            self.rect.x += self.moveSpeed
         if not keys[pygame.K_w] and not keys[pygame.K_a] and not keys[pygame.K_s] and not keys[pygame.K_d]:
             self.image = pygame.transform.scale_by(pygame.image.load('Images/Player/' + playerDirection + '/' + playerDirection + '.png'), 10)
         else:
             self.image = pygame.transform.scale_by(pygame.image.load('Images/Player/' + playerDirection + '/' + playerDirection + str(animationCycle) + '.png'), 10)
         return playerDirection
-    def slash(self):
-        if playerDirection == 'Up':
-            return self.rect.x, self.rect.y + 10
-        elif playerDirection == 'Down':
-            return self.rect.x, self.rect.y - 10
-        elif playerDirection == 'Right':
-            return self.rect.x + 10, self.rect.y
-        elif playerDirection == 'Left':
-            return self.rect.x + 10, self.rect.y
