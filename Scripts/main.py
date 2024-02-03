5ca8b4a3a6f8ecf673cec78e926de8d216aeba13
Max <supkittymeow@gmail.com>
2024-02-02 18:05:04 -0800

    adds stuff

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,1 +1,16 @@
-Hi!
+import pygame
+
+SCREEN_WIDTH = 340
+SCREEN_HEIGHT = 560
+screen = pygame.display.get_active()
+
+pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.init()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
