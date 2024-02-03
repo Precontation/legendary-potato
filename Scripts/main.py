f73e05a01e3a1f60b51569bfcf4df8a9240a8724
Max <supkittymeow@gmail.com>
2024-02-02 18:35:06 -0800

    asdf

--- Scripts/main.py
+++ Scripts/main.py
@@ -3,18 +3,22 @@ import player
 
 SCREEN_WIDTH = 340
 SCREEN_HEIGHT = 560
-screen = pygame.display.get_active()
-
-pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.init()
 
 character = player.Player()
+animationCycle = 0
+shouldChangeAnim = 0
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # screen.blit(character.image, (character.rect.x, character.rect.y))
+    screen.fill('black')
+
+    character.move(pygame.key.get_pressed(), character.playerDirection, animationCycle, character.keysPressed)
+    screen.blit(character.image, (character.rect.x, character.rect.y))
+    pygame.display.flip()
 pygame.quit()
