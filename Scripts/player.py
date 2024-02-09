557cd8486ef814ae1eefc68c5a38dbd4cf2961df
Max <supkittymeow@gmail.com>
2024-02-08 17:03:49 -0800

    Start

--- Scripts/player.py
+++ Scripts/player.py
@@ -28,6 +28,9 @@ class Player(pygame.sprite.Sprite):
             return world_y
         
     def move(self, keys, playerDirection, moveAnimationCycle, idleAnimationCycle):
+        oldPlayerDir = playerDirection
+        hasSwitched = False
+
         if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.playerDirection = 'Up'
             self.rect.y -= self.moveSpeed
@@ -40,7 +43,14 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.playerDirection = 'Right'
             self.rect.x += self.moveSpeed
+            
+        if ((keys[pygame.K_a] or keys[pygame.K_LEFT]) and (keys[pygame.K_d] or keys[pygame.K_RIGHT])) and (self.playerDirection == 'Right' or self.playerDirection == 'Left'):
+            self.playerDirection = oldPlayerDir
+        if ((keys[pygame.K_w] or keys[pygame.K_UP]) and (keys[pygame.K_s] or keys[pygame.K_DOWN])) and (self.playerDirection == 'Up' or self.playerDirection == 'Down'):
+            self.playerDirection = oldPlayerDir
+
         if not keys[pygame.K_w] and not keys[pygame.K_UP] and not keys[pygame.K_a] and not keys [pygame.K_LEFT] and not keys[pygame.K_s] and not keys[pygame.K_DOWN] and not keys[pygame.K_d] and not keys[pygame.K_RIGHT]:
+            if not (keys[pygame.K_a] and keys[pygame.K_d]) or (keys[pygame.w])
             self.image = pygame.transform.scale_by(pygame.image.load('Images/Player/' + playerDirection + '/Idle' + str(idleAnimationCycle) + '.png'), 5)
         else:
             self.image = pygame.transform.scale_by(pygame.image.load('Images/Player/' + playerDirection + '/Moving' + str(moveAnimationCycle) + '.png'), 5)
